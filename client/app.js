bb4f7d9f36e08329cdd571f4e1a6613de5273d2a
Tarek <tarek.alkhatib1@gmail.com>
2018-06-22 14:41:13 +0200

    PoachDB created

--- client/app.js
+++ client/app.js
@@ -1,17 +1,45 @@
 // Create a local PouchDB database
-const db = new PouchDB("Button");
+const db = new PouchDB('my_database');
 console.log("Local database created");
 
-
-function addTodo(text) {
-  var todo = {
+function sosSend(data) {
+  const sos = {
     _id: new Date().toISOString(),
-    title: text,
-    completed: false
+    destLocation: data,
+    locatoin: "",
+    currTime: "",
+    estTravelTime: "",
+    email: "",
+    image:"",
+    message: "",
+    name: "",
+    status:""
   };
-  db.put(todo, function callback(err, result) {
+  db.put(sos, function callback(err, result) {
     if (!err) {
-      console.log('Successfully posted a todo!');
+      console.log('Successfully SOS is sended!');
     }
   });
 }
+
+document.getElementById("submit").addEventListener("click", function(e){
+  e.preventDefault();
+  const data = {
+   destLocation: document.getElementsByName("destLocation")[0].value,
+   locatoin: document.getElementsByName("destLocation")[0].value,
+   currTime: document.getElementsByName("destLocation")[0].value,
+   estTravelTime: document.getElementsByName("destLocation")[0].value,
+   email: document.getElementsByName("destLocation")[0].value,
+   image: document.getElementsByName("destLocation")[0].value,
+   message: document.getElementsByName("destLocation")[0].value,
+   name: document.getElementsByName("destLocation")[0].value,
+   status: document.getElementsByName("destLocation")[0].value
+}
+  document.getElementById("demo").innerHTML = data.destLocation;
+  sosSend(data);
+});
+
+
+
+
+
