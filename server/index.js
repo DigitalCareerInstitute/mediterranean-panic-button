fa3d098ee94b77b1bed5ce0ef87680f75cdb6a59
laithmassoud <laythmassoud8@gmail.com>
2018-06-22 13:42:31 +0200

    some changes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const cron = require('node-cron');
+
 
 
 app.get('/', (req, res, next) => {
     res.json({api:"Welcome to our App"})
 })
+
+cron.schedule('1,2,4,5 * * * *', function(){
+  console.log('running every minute 1, 2, 4 and 5');
+});
+
 
 
 
